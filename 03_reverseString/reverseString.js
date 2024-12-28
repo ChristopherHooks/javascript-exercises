6abb99c8b091c5621a4fa5be021b3756e4f43e14
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-28 10:21:56 -0600

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let origString = [];
+    origString = string.split('');
 
+    let revString = [];
+    let output = '';
+
+    for (let i = 0; i < origString.length; i++) {
+        revString.unshift(origString[i]);
+    }
+
+    for (let i = 0; i < revString.length; i++) {
+        output += revString[i];
+    }
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
