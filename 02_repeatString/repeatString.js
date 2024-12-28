be3f220937613c27edb562a1c8de4f86b20ca2f6
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-28 09:56:04 -0600

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    let output = '';
 
-};
+    if (num < 0) {
+        output = 'ERROR';
+    }
+
+    for (let i = 0; i < num; i++) {
+        output += string;
+    }
+
+    return output;
+    // console.log(output);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
