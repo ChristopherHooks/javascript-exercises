e236fc6ed2b75f8a9b051aaa20e277b45747f241
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-29 17:51:10 -0600

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,25 @@
 const removeFromArray = function (array, ...args) {
 
+    let removalArr = [];
 
-    for (let argument of args) {
-        let position = array.indexOf(argument);
+    for (let i = 0; i < array.length; i++) { //Step through array
 
-        array.splice(position, 1);
+        for (let argument of args) {         //Step through arguments
+            if (array[i] === argument) {    //If argument matches item add index to array
+                removalArr.push(array.indexOf(argument)); //Add index to removal array
+            }
+        }
     }
 
+    while (removalArr.length > 0) {         //Step through removal arr
+        array.splice(removalArr.pop(), 1);  //Remove popped value from array
+    }
 
     // console.log(array);
+    // console.log(removalArr);
     return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// ([1, 2, 3, 4, 5, 6], 2, 4)
