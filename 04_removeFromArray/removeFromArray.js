e03f7c9cafebdbc5e94defb0fe897c785f83aad4
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-29 17:52:06 -0600

    Remove unnecessary comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,24 +2,22 @@ const removeFromArray = function (array, ...args) {
 
     let removalArr = [];
 
-    for (let i = 0; i < array.length; i++) { //Step through array
+    for (let i = 0; i < array.length; i++) {
 
-        for (let argument of args) {         //Step through arguments
-            if (array[i] === argument) {    //If argument matches item add index to array
-                removalArr.push(array.indexOf(argument)); //Add index to removal array
+        for (let argument of args) {
+            if (array[i] === argument) {
+                removalArr.push(array.indexOf(argument));
             }
         }
     }
 
-    while (removalArr.length > 0) {         //Step through removal arr
-        array.splice(removalArr.pop(), 1);  //Remove popped value from array
+    while (removalArr.length > 0) {
+        array.splice(removalArr.pop(), 1);
     }
 
-    // console.log(array);
-    // console.log(removalArr);
+
     return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
-// ([1, 2, 3, 4, 5, 6], 2, 4)
