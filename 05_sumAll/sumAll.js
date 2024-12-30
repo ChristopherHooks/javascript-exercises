d042f91a355cc1077ac3e8dfd2358912eaeaf270
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-29 18:20:09 -0600

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let orig = num1;
+    let end = num2;     //Set origin and end points
+    let sum = 0;
 
-};
+    if (
+        (num1 < 0 || num2 < 0) ||
+        (num1 % 1 !== 0 || num2 % 1 !== 0) ||
+        (num1 === NaN || num2 === NaN) ||
+        (typeof num1 !== 'number' || typeof num2 !== 'number')
+    ) {
+        return 'ERROR';
+    }
 
+    if (orig < end) {
+        for (let i = orig; i <= end; i++) {
+            sum += i;
+        }
+    }
+
+    else if (orig > end) {
+        for (let i = orig; i >= end; i--) {
+            sum += i;
+        }
+    }
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
