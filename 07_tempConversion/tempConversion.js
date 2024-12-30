ccafa195eee83de08e8c454f5cebf7728a45f4ef
Christopher Hooks <christopher.hooks89@gmail.com>
2024-12-29 19:04:58 -0600

    Complete Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celcius = (temp - 32) * (5 / 9);
+
+  if (
+    ((temp - 32) * (5 / 9) > 0) ||
+    ((temp - 32) * (5 / 9) < 0)
+  ) {
+    celcius = celcius.toFixed(1);
+    celcius = parseFloat(celcius);
+  }
+
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = (temp * (9 / 5) + 32);
+
+  if (
+    ((temp * (9 / 5) + 32) > 0) ||
+    ((temp * (9 / 5) + 32) < 0)
+  ) {
+    fahrenheit = fahrenheit.toFixed(1);
+    fahrenheit = parseFloat(fahrenheit);
+  }
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
