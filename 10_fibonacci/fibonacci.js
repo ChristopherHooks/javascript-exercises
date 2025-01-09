4fc01d587c21bc3924ab710c2204624bed3ab2e2
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-09 09:52:12 -0600

    Complete exercise 10: Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    const fibArray = [0, 1];
 
+    for (let i = 2; i <= count; i++) {
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fib[count];
 };
 
 // Do not edit below this line
