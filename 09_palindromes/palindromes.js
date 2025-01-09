21806ca5d57125dc11a23d13b69c484ce024b15a
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-09 08:27:21 -0600

    Complete exercise 9: Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //convert string to array
+    //letters only
+    const cleanString = string
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, '');
 
+    //reverse array
+    const reverseString = cleanString.split('').reverse().join('');
+
+    //compare 
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
