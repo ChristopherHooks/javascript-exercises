2cd1c357e90fa95b19c3380682f58139e208ecbc
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-08 21:55:27 -0600

    Complete exercise 8: Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.lenth === 0) {
+    return 0;
+  } else {
+    return array.reduce((total, num) => total += num, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  let total = 1;
+  for (let i = 0; i < power; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let total = 1;
+  while (n > 0) {
+    total *= n;
+    n--;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
